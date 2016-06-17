aa0fa976477c8435989598643ab6de8cbb27da7e
Scar <boxhaxor@gmail.com>
2016-06-16 23:16:31 -0600

    Added bootstrap for real.

--- Source/Presentation/Presentation.Beer.Registrar/App_Start/BundleConfig.cs
+++ Source/Presentation/Presentation.Beer.Registrar/App_Start/BundleConfig.cs
@@ -11,7 +11,14 @@ namespace Presentation.Beer.Registrar
             //bundles.Add(new StyleBundle("~/Content/css").Include(
             //          "~/Content/bootstrap.css",
             //          "~/Content/site.css"));
-
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+                .Include("~/node_modules/jquery/dist/jquery.min.js")
+                .Include("~/node_modules/components-bootstrap/js/bootstrap.min.js")
+                .Include("~/node_modules/components-bootstrap/js/transition.js")
+                .Include("~/node_modules/components-bootstrap/js/collapse.js")
+                .Include("~/node_modules/components-bootstrap/js/modal.js")
+                .Include("~/node_modules/components-bootstrap/js/dropdown.js")
+                );
             bundles.Add(new ScriptBundle("~/bundles/styles").Include(
                       "~/Scripts/Styles/style.js"));
 
